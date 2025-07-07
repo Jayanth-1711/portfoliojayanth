448702642118a7747b59e30ca91fcb1fb9047e0d
Jayanth-1711 <venkatajayanth1711@gmail.com>
2025-07-07 21:53:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+let navProject = document.getElementById("navProject");
+let navCertificate = document.getElementById("navCertificate");
+let navSkill = document.getElementById("navSkill");
+document.getElementById('section1').style.display = 'block';
+function projectEvent(){
+    navProject.classList.add("nav-select");
+    navCertificate.classList.remove("nav-select");
+    navSkill.classList.remove("nav-select");
+
+}
+function certificateEvent(){
+    navCertificate.classList.add("nav-select");
+    navProject.classList.remove("nav-select");
+    navSkill.classList.remove("nav-select");
+}
+
+function skillsEvent(){
+    navSkill.classList.add("nav-select");
+    navCertificate.classList.remove("nav-select");
+    navProject.classList.remove("nav-select");
+
+}
+
+function display(sectionId) {
+  // Hide all sections
+  document.getElementById('section1').style.display = 'none';
+  document.getElementById('section2').style.display = 'none';
+
+  // Show the clicked section
+  document.getElementById(sectionId).style.display = 'block';
+}
